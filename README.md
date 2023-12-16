1c34494cc6e8fee59c613ae769a7b9ad53194193
Luisa Orozco <l.orozco@esciencecenter.nl>
2023-12-16 14:31:56 +0100

    First commit, add files

---
+++ README.md
@@ -0,0 +1,39 @@
+## Installation
+
+```bash
+ conda create --name tuya python=3.9
+ conda activate tuya
+ python3 -m pip install python-crypto          
+ python3 -m pip install pycryptodome            # or pycrypto, pyaes, Crypto
+ python3 -m pip install tinytuya                # or pytuya
+ python3 -m pip install tuyapower               # Pull this tuyapower module from PyPi
+ python3 -m pip install demjson3                # Read json files
+ python3 -m pip install matplotlib              # do some plotting 
+```
+
+## Setup of TuyaPower
+
+Follow the instructions for [Tuya Device preparation from TuyaPower](https://github.com/jasonacox/tuyapower#tuya-device-preparation) and read the **Notes**. They are pertinent for using these scripts after the first time.
+
+As a result, you should now have in the root folder the following files:
+
+- `devices.json` : most important one - contains the keys.
+- `tinytuya.json` : configuration data with the credentials that you used to scan.
+- `tuya-raw.json` : General information about the devices, contains the mappings.
+- `snapshot.json` : Only created if you answered `Y` to Poll local devices? (Y/n). It has the current reading values of the device.
+
+## Scripts
+
+This contains examples scripts for using `tuyapower`.
+
+Check the main [Example.py](https://github.com/jasonacox/tuyapower/blob/master/examples/README.md) from TuyaPower.
+
+### washing_machine.py
+
+I created this script to monitor the power consumption during a wash.
+This script:
+
+- Gathers the device info and establishes the communication with it.
+- Has a loop in time to retrieve the information from the smart plug.
+- Saves the reading to a txt file with name the time stamp.
+- (Optionally) plots the power consumption.
---
+++ scripts/washing_machine.py
@@ -0,0 +1,80 @@
+"""
+ Notes from Luisa Orozco:
+ - We have two networks LuisaHerman and Luisa&Herman. The devices on the second floor are in LuisaHerman network. So this script should be run on a machine connected to the right network (LuisaHerman).
+"""
+import os
+import time
+from matplotlib import pyplot as plt
+import demjson3 as demjson
+import tuyapower
+
+
+# Terminal Color Formatting
+bold="\033[0m\033[97m\033[1m"
+subbold="\033[0m\033[32m"
+normal="\033[97m\033[0m"
+dim="\033[0m\033[97m\033[2m"
+alert="\033[0m\033[91m\033[1m"
+alertdim="\033[0m\033[91m\033[2m"
+
+PLOT_REAL_TIME = False
+
+# Load Device Keys from Tuya JSON file
+print("Loading Tuya Keys...")
+f = open('../../devices.json',"r")
+data = demjson.decode(f.read())
+f.close()
+print("    %s%s device keys loaded%s"%(subbold, len(data), normal))
+
+print("Polling devices...")
+for i in data:
+    name = i['name'] 
+    if name != 'washing machine': continue # only consider device with name 'washing machine'
+    if (i['ip'] == 0):
+        print ('%s[%s]%s - %sError - No IP found%s'%(bold, name, dim, alert, normal))
+    else:
+        if PLOT_REAL_TIME:
+            fig1 = plt.figure()
+            plt.xlabel('Time (s)')
+            plt.ylabel('Power (kWh)')
+            plt.title('Power vs Time')
+        os.makedirs('../measurements/', exist_ok=True)
+        output_file = open('../measurements/' 
+                           + time.strftime("%Y-%m-%d_%H-%M", time.localtime())
+                           + '.txt', 'w')
+        output_file.write("%s\t%s\t%s\t%s\t%s\n"%("#Time", "W", "mA", "V","kWh"))
+        times, watts, power_consumption = [], [], []
+        time_start = time.time()
+        time_iteration = time_start
+        total_measure_time = 60 * 3 # minutes
+        while (time.time() - time_start < total_measure_time*60):
+            (on, w, mA, V, err) = tuyapower.deviceInfo(i['id'], i['ip'], i['key'], i['version'])
+            times.append(time.time() - time_start)
+            kwh = (w/1000) * (time.time() - time_iteration)/3600
+            time_iteration = time.time()
+            power_consumption.append(kwh)
+            watts.append(w)
+            state = alertdim + "Off" + normal
+            if isinstance(on, dict):
+                state = dim + "%d Switches: " % len(on)
+                for e in on:
+                    if(on[e] == True):
+                        state = state + normal + e + ":" + subbold + "On " + normal
+                    else:
+                        state = state + normal + e + ":" + alertdim + "Off " + normal
+            elif(on):
+                state = subbold + "On" + normal
+            if(err == "OK"):
+                print("%s[%s]%s - %s - Power: %sW, %smA, %sV %skWh"%(bold, name, normal, state, w, mA, V, kwh))
+                output_file.write("%s\t%s\t%s\t%s\t%s\n"%(time.time() - time_start, w, mA, V, kwh))
+            else:
+                print("%s[%s]%s - %s"%(bold, name, normal, state))
+            if PLOT_REAL_TIME:
+                #plotting the read values
+                plt.plot(times, power_consumption, 'k')
+                #plt.show()
+                plt.draw()
+                plt.pause(0.001)
+            time.sleep(10) # wait X seconds before polling again
+        output_file.close()
+print("END")
